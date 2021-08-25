d038374d73fa6a3b094f377830e0bd8d2fdc6e62
HoneyJain11 <hrjain911@gmail.com>
2021-08-25 20:56:20 +0530

    Added Basic UI.

---
+++ Assets/Scripts/LobbyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyController : MonoBehaviour
+{
+    public Button playButton;
+    public Button quitButton;
+    public string newScene;
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(StartPlay);
+        quitButton.onClick.AddListener(Quit);
+    }
+
+    public void StartPlay()
+    {
+        SceneManager.LoadScene(newScene);
+    }
+
+    public void Quit()
+    {  
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PauseResumeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseResumeScript : MonoBehaviour
+{
+    //public Button btnPause;
+    bool isPaused = false;
+
+    public void PauseGame()
+    { 
+        if (isPaused == true)
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+
+        }
+
+        else
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+    }
+
+}
